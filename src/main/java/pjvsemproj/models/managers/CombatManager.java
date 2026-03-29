012bcd4b7f0d3784754e91b8c60e6af8703b326b
vasilcoin002 <vasilcoin002@gmail.com>
2026-03-30 01:01:16 +0200

    CombatManager created & units are healing in city on every player's turn created

---
+++ src/main/java/pjvsemproj/models/managers/CombatManager.java
@@ -0,0 +1,47 @@
+package pjvsemproj.models.managers;
+
+import pjvsemproj.models.entities.troopUnits.TroopUnit;
+import pjvsemproj.models.game.players.Player;
+import pjvsemproj.models.maps.Tile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombatManager implements ITurnListener{
+
+    private Player currentPlayer;
+
+    @Override
+    public void onTurnStart(Player activePlayer) {
+        currentPlayer = activePlayer;
+
+
+        getTroopsToHeal().forEach(troopUnit -> {
+            int heal = (int) (troopUnit.getMaxHealth() * 0.25);
+            troopUnit.takeHeal(heal);
+        });
+    }
+
+    @Override
+    public void onTurnEnd(Player endingPlayer) {
+
+    }
+
+    // TroopUnit gets heal if it stands in city on every player's turn
+    public List<TroopUnit> getTroopsToHeal() {
+        List<TroopUnit> troopsToHeal = new ArrayList<>();
+
+        currentPlayer.getCities().forEach(city -> {
+            Tile cityTile = city.getTile();
+            cityTile.getEntities().forEach(entity -> {
+                if (entity instanceof TroopUnit troopUnit) {
+                    if (troopUnit.getOwner() == currentPlayer) {
+                        troopsToHeal.add(troopUnit);
+                    }
+                }
+            });
+        });
+
+        return troopsToHeal;
+    }
+}
