a618c4a501c3c23e4e023cfa51766cb297071af3
vasilcoin002 <vasilcoin002@gmail.com>
2026-04-06 02:44:30 +0200

    HpRenderer renderEntityHp renders hp

--- src/main/java/pjvsemproj/views/renderers/HpRenderer.java
+++ src/main/java/pjvsemproj/views/renderers/HpRenderer.java
@@ -11,6 +11,9 @@ import static pjvsemproj.views.ViewConstants.TILE_SIZE;
 
 public class HpRenderer extends Renderer {
 
+    int boxXSize = 4;
+    int boxYSize = 40;
+
     public HpRenderer(Canvas canvas) {
         super(canvas);
     }
@@ -19,16 +22,22 @@ public class HpRenderer extends Renderer {
         int viewX = getEntityViewX(entity);
         int viewY = getEntityViewY(entity);
 
-        // TODO finish later
+        int boxXPos = viewX + TILE_SIZE - boxXSize;
+        int boxYPos = viewY + (TILE_SIZE - boxYSize) / 2;
+
+        gc.setFill(Color.RED);
+        gc.fillRect(boxXPos, boxYPos, boxXSize, boxYSize);
+
+        // TODO finish later when entity.getMaxHealth will be in interface
 //        double percentHpLeft = entity.getHealth() / entity.getMaxHealth();
+        double percentHpLeft = 0.25;
 
         gc.setFill(Color.GRAY);
-        gc.fillRect(viewX + TILE_SIZE - 4, viewY + 12, 4, 40);
-
+        gc.fillRect(boxXPos, boxYPos, boxXSize, (1 - percentHpLeft) * boxYSize);
     }
 
     public <T extends Damageable & IGridEntity> void renderEntitiesHp(List<? extends T> entities) {
-        for (T entity: entities) {
+        for (T entity : entities) {
             renderEntityHp(entity);
         }
     }
