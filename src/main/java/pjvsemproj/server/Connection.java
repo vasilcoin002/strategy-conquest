a4e17fb8f5eb51695205c0e5dae719fb17bd42ae
hryshiva <hryshiva@fel.cvut.cz>
2026-04-11 00:34:49 +0200

    added basic logic into GameServer and Connection

--- src/main/java/pjvsemproj/server/Connection.java
+++ src/main/java/pjvsemproj/server/Connection.java
@@ -1,4 +1,19 @@
 package pjvsemproj.server;
 
-public class Connection {
+import java.net.Socket;
+
+public class Connection implements Runnable{
+
+    private final GameServer server;
+    private final Socket socket;
+
+    public Connection(GameServer server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
--- src/main/java/pjvsemproj/server/GameServer.java
+++ src/main/java/pjvsemproj/server/GameServer.java
@@ -1,4 +1,32 @@
 package pjvsemproj.server;
 
-public class GameServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GameServer implements Runnable {
+
+    private final int port;
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+
+    public GameServer(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true){
+                socket = serverSocket.accept();
+                Connection connection = new Connection(this, socket);
+
+                new Thread(connection).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
