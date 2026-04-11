0a0a9915fd8fa087ebba33c6f2e3e62532b7e2fc
hryshiva <hryshiva@fel.cvut.cz>
2026-04-11 23:40:54 +0200

    upgraded classes Connection && GameServer, added GameSession

--- src/main/java/pjvsemproj/server/Connection.java
+++ src/main/java/pjvsemproj/server/Connection.java
@@ -1,11 +1,27 @@
 package pjvsemproj.server;
 
+import pjvsemproj.models.game.players.Player;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Connection implements Runnable{
 
+    // TODO handleMove, handleAttack, handleBuyUnit, handleUpgradeCity, handleEndTurn
+
+    private static final Logger LOGGER = Logger.getLogger(Connection.class.getName());
     private final GameServer server;
     private final Socket socket;
+    private Player player;
+    private String playerName;
+    private BufferedReader in;
+    private PrintWriter out;
+    private GameSession session;
 
     public Connection(GameServer server, Socket socket) {
         this.server = server;
@@ -14,6 +30,120 @@ public class Connection implements Runnable{
 
     @Override
     public void run() {
+        try{
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
 
+            boolean running = true;
+            while (running){
+                String msg = in.readLine();
+                LOGGER.log(Level.INFO, "Server received from {0}: >>>{1}<<<",
+                        new Object[]{playerName, msg});
+                if(msg != null){
+                    running = processIncomingMessage(msg);
+                } else {
+                    running = false;
+                }
+            }
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, "Error communicating with client {0}",
+                    ex.getMessage());
+        } finally {
+            quit();
+        }
+    }
+
+    private boolean processIncomingMessage(String msg){
+        String[] tokens = msg.split("\\|", -1);
+        Protocol actionCode = Protocol.valueOf(tokens[0]);
+
+        switch(actionCode){
+            case LOGIN:
+
+        }
+    }
+
+    public void sendToClient(Protocol code, String... args){
+        StringBuilder msg = new StringBuilder(code.toString());
+
+        for (String arg : args) {
+            msg.append("|").append(arg);
+        }
+
+        out.println(msg);
+    }
+
+    private boolean handleLogin(String[] tokens){
+        if (tokens.length < 2){
+            sendToClient(Protocol.ERROR, "LOGIN_REQUIRES_NAME");
+            return true;
+        }
+        String requestedName = tokens[1].trim();
+        boolean accepted = server.registerConnection(this, requestedName);
+        if (!accepted) {
+            sendToClient(Protocol.ERROR, "NAME_ALREADY_TAKEN");
+            return true;
+        }
+        sendToClient(Protocol.OK, "LOGIN_ACCEPTED");
+        return true;
+    }
+
+    private boolean handleReady() {
+        if (!isLoggedIn()) {
+            sendToClient(Protocol.ERROR, "NOT_LOGGED_IN");
+            return true;
+        }
+
+        if (session == null) {
+            sendToClient(Protocol.ERROR, "NOT_IN_SESSION");
+            return true;
+        }
+
+        session.handleReady(this);
+        return true;
+    }
+
+    private boolean handleMove(String[] tokens){
+
+        if(!isLoggedIn()){
+            sendToClient(Protocol.ERROR, "NOT_LOGGED_IN");
+        }
+
+        if (tokens.length < 4) {
+            sendToClient(Protocol.ERROR, "MOVE_REQUIRES_UNITID_X_Y");
+            return true;
+        }
+        String unitId = tokens[1];
+
+        try{
+            int x = Integer.parseInt(tokens[2]);
+            int y = Integer.parseInt(tokens[3]);
+            session.handleMove();
+        } catch (NumberFormatException ex){
+            sendToClient(Protocol.ERROR, "INVALID_COORDINATES");
+        }
+
+        return true;
+    }
+
+    private boolean handleMove(){}
+    private boolean handleAttack(){}
+    private boolean handleBuyUnit(){}
+    private boolean handleUpgradeCity(){}
+    private boolean handleEndTurn(){}
+
+    private boolean isLoggedIn() {
+        return player != null;
+    }
+
+    private void quit() {
+        LOGGER.info("Quitting connection.");
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException ex) {
+            LOGGER.severe(ex.getMessage());
+        }
     }
 }
--- src/main/java/pjvsemproj/server/GameServer.java
+++ src/main/java/pjvsemproj/server/GameServer.java
@@ -3,22 +3,31 @@ package pjvsemproj.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 public class GameServer implements Runnable {
+    // TODO unregisterConnection, stopServer, tryAssignToSession, removeSession
 
-    private final int port;
+
+    private final int PORT_NUMBER;
     private ServerSocket serverSocket;
     private Socket socket;
-
+    private final Map<String, Connection> connectionsByName = new HashMap<>();
+    private final List<GameSession> sessions = new ArrayList<>();
+    private static final Logger LOGGER = Logger.getLogger(GameServer.class.getName());
 
     public GameServer(int port) {
-        this.port = port;
+        this.PORT_NUMBER = port;
     }
 
     @Override
     public void run() {
         try {
-            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket = new ServerSocket(PORT_NUMBER);
             while (true){
                 socket = serverSocket.accept();
                 Connection connection = new Connection(this, socket);
@@ -29,4 +38,26 @@ public class GameServer implements Runnable {
             throw new RuntimeException(e);
         }
     }
+
+    public synchronized boolean registerConnection(Connection connection, String name) {
+        if(isNameTaken(name)){
+            LOGGER.info("Adding connection for" + name);
+            connectionsByName.put(name, connection);
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized boolean isNameTaken(String name){
+        if(connectionsByName.containsKey(name)){
+            return false;
+        }
+        return true;
+    }
+
+    public synchronized void unregisterConnection(){}
+    public synchronized void stopServer(){}
+    public synchronized void tryAssignToSession(){}
+    public synchronized void removeSession(){}
+
 }
