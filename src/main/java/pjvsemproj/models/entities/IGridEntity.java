bc06a9cc47b09dd805eb79b2177f1fe169895005
hryshiva <hryshiva@fel.cvut.cz>
2026-04-27 23:54:03 +0200

    fixed IGridEntity

--- src/main/java/pjvsemproj/models/entities/IGridEntity.java
+++ src/main/java/pjvsemproj/models/entities/IGridEntity.java
@@ -6,7 +6,6 @@ import pjvsemproj.models.game.maps.Tile;
  * Represents an object that can be placed on a map tile.
  */
 public interface IGridEntity {
-    String getId();
     Tile getTile();
     void setTile(Tile tile);
     boolean isPassable();
