15650578df461e24f15f94be58b9460ef9f335f2
hryshiva <hryshiva@fel.cvut.cz>
2026-05-12 17:18:07 +0200

    Added ServerEventListener

---
+++ src/main/java/pjvsemproj/server/ServerEventListener.java
@@ -0,0 +1,18 @@
+package pjvsemproj.server;
+
+public interface ServerEventListener {
+
+    void onUnitMoved(String unitId, int x, int y);
+
+    void onUnitAttacked(
+            String attackerId,
+            String targetId,
+            int newHp
+    );
+
+    void onUnitDied(String unitId);
+
+    void onTurnStarted(String playerName);
+
+    void onCityUpgraded(String cityId);
+}
