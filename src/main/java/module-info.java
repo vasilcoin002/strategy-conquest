0d724796d74dae0e976b5a179f77f82380b6ea31
vasilcoin002 <vasilcoin002@gmail.com>
2026-05-18 03:33:51 +0200

    add: product documentation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ module pjvsemproj.pjvsemproj {
     requires javafx.graphics;
     requires java.logging;
     requires com.google.gson;
-    requires pjvsemproj.pjvsemproj;
 
     opens pjvsemproj to javafx.fxml;
     exports pjvsemproj;
